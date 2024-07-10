68762d343d0e38ae71829c6a2dacb9adac1d65d9
221300012-Xiang Yuanfang <yf.xiang@smail.nju.edu.cn>
2024-07-10 13:40:38 +0800

    Jul 10 Combined concepts to dataset

---
+++ src/data/datasetCombine.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import os
+import re
+
+matrix = []
+pattern = r'SO_*A*\d+'
+
+def id_regu(s):
+    res = re.findall(pattern, s) 
+    if len(res) != 1:
+        return np.NaN
+
+    if res[0].find('_') == -1:
+        res[0] = 'SO_'+res[0][2:]
+
+    return res[0]
+
+
+for f in os.listdir('dataset/matrix'):
+    df = pd.read_csv('dataset/matrix/'+f)
+
+    df = df.drop('Unnamed: 0', axis=1)
+    df['GENE'] = df['GENE'].apply(id_regu)
+
+    df = df.dropna(subset=['GENE'])
+    df.rename(columns={'GENE':'SAMPLES'}, inplace=True)
+    df = df.groupby('SAMPLES').mean()
+    #df.set_index(keys='GENE')
+
+    # TODO: regularize gene ID
+    #print(df)
+    matrix.append(df)
+
+df = pd.concat(matrix, axis=1)
+df = df.transpose()
+
+con_df = pd.read_csv('dataset/GSE_concepts.csv')
+con_df.set_index('SAMPLES', inplace=True)
+print(con_df)
+
+df = df.reindex()
+df.sort_index(inplace=True)
+#df.set_index('SAMPLES', inplace=True)
+
+con_df = con_df.loc[df.index]
+con_df.drop_duplicates(inplace=True)
+df.insert(0, 'CONCEPTS', con_df['CONCEPTS'], allow_duplicates=True)
+
+print(df)
+df.to_csv('dataset/dataset.csv')
