a4148477eda94ae85a181c4434fa4733291d7a0e
RAINYWJT <798250397@qq.com>
2024-07-14 19:25:49 +0800

    Process-data-with-mean

---
+++ src/pca/pca_with_mean.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('../../dataset/new_dataset.csv')
+concept_column = df['CONCEPTS']
+concept_vectors = df.drop(columns=['CONCEPTS', 'CONCEPT'])
+concept_groups = df.groupby('CONCEPTS')
+concept_dict = {}
+
+for concept, group in concept_groups:
+    vectors = group.drop(columns=['CONCEPTS', 'CONCEPT']).values
+    concept_dict[concept] = vectors
+
+average_vectors = {}
+for concept, vectors in concept_dict.items():
+    average_vectors[concept] = np.mean(vectors, axis=0)
+result_rows = []
+
+for concept, vector in average_vectors.items():
+    result_rows.append([concept] + vector.tolist())
+
+column_names = ['CONCEPTS'] + [f'vector{i+1}' for i in range(100)]
+result_df = pd.DataFrame(result_rows, columns=column_names)
+
+result_df.to_csv('../../dataset/reduced_dataset.csv', index=False)
+
+print("平均向量计算完成，结果已保存到 reduced_dataset.csv")
---
+++ src/pca/preprocess.py
@@ -0,0 +1,55 @@
+import re
+
+# Process the dataset
+def split_data(line):
+    pattern = re.compile(r'(http://purl\.obolibrary\.org/obo/GO_\d+)\s+([-.\de\s]+)')
+    match = pattern.match(line)
+    if match:
+        url = match.group(1)
+        values = match.group(2).split()
+        return url, values
+    return None, None
+
+def process_file(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as input_file:
+        lines = input_file.readlines()
+    
+    with open(output_file_path, 'w') as output_file:
+        for line in lines:
+            url, values = split_data(line)
+            if url and values:
+                output_file.write(f"{url} {' '.join(values)}\n")
+
+input_file_path = '../../dataset/ontology.embeddings.txt'  
+output_file_path = '../../dataset/embeddings_preprocess.txt'  
+process_file(input_file_path, output_file_path)
+
+# Check the dataset
+def count_data_values(file_path):
+    pattern = re.compile(r'(http://purl\.obolibrary\.org/obo/GO_\d+)\s+([-.\de\s]+)')
+    count = 0
+    with open(file_path, 'r') as file:
+        for line in file:
+            match = pattern.match(line)
+            if match:
+                url = match.group(1)
+                values = match.group(2).split()
+                print(f"URL {url} has {len(values)} values.")
+                count += 1
+            else:
+                assert("No!")
+    return count
+
+
+# Change it
+file_path = '../../dataset/embeddings_preprocess.txt'
+output_file_path = '../../dataset/embeddings_preprocess_modified.txt'
+
+with open(file_path, 'r') as file, open(output_file_path, 'w') as output_file:
+    for line in file:
+        line = line.replace('http://purl.obolibrary.org/obo/', '')
+        line = line.replace('GO_', 'GO:')
+        output_file.write(line)
+
+print_count = count_data_values(file_path)
+print(f"Total print statements executed: {print_count}")
