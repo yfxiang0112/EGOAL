c23c8efecfc0a50f66e93d381e30b1e40d69f6fd
RAINYWJT <798250397@qq.com>
2024-07-15 20:45:42 +0800

    New_dataprocess

---
+++ src/pca/check_dataset_concepts.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+processed_df = pd.read_csv('../../dataset/processed_dataset.csv')
+processed_concepts = processed_df['CONCEPTS']
+
+reduced_df = pd.read_csv('../../dataset/reduced_dataset.csv')
+reduced_concepts = reduced_df['CONCEPTS']
+
+print(len(processed_concepts),len(reduced_concepts))
+
+for i in range(len(processed_concepts)):
+    flag = 0
+    for j in range(len(reduced_concepts)):
+        if processed_concepts[i] == reduced_concepts[j]:
+            flag = 1
+    if flag == 0:
+        print('diff!')
+        assert(0)
+    print(flag, i)
