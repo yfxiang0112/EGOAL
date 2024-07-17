76c6d9549249845f7d0c87d8094dded5c943242e
RAINYWJT <798250397@qq.com>
2024-07-17 11:19:27 +0800

    Change of KB in ABL

--- abl/kb.py
+++ abl/kb.py
@@ -12,12 +12,30 @@ class GO(KBBase):
         # TODO()
 
         # Define the variables and rule
+        rules = []
         # TODO()
 
         # Define the weights and violated weights
+        self.weights = {rule: 1 for rule in rules}
+        self.total_violation_weight = Sum(
+            [If(Not(rule), self.weights[rule], 0) for rule in self.weights]
+        )
         # TODO()
+
 
     def logic_forward(self, pseudo_label, data_point):
         # Define the logical rules
         # TODO()
+
+        # Steps as follow:
+        # 1.Initial the variable 
+        
+        # 2.Reset the solver
+
+        # 3.Add the attribute restrictions
+
+        # 4.Add the aim restrictions
+
+        # 5.Check the state of solver
+
         return 
