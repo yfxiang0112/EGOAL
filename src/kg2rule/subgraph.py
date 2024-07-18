1e10b45ca00f50c43f0a3ec15c4b8c2ad3e58079
221300012-Xiang Yuanfang <yf.xiang@smail.nju.edu.cn>
2024-07-18 10:42:22 +0800

    Jul 18 added predicate extraction & filtered OWL graph

--- src/kg2rule/subgraph.py
+++ src/kg2rule/subgraph.py
@@ -9,29 +9,40 @@ def findID(s):
 
     s = str(s)
 
+    idx = 0
+    for i in range(len(s)):
+        if s[i] == '/':
+            idx = i
+    s = s[idx+1:]
+    return s
+
+
+    '''
     res = re.findall(r'GO_\d*', s)
     if len(res) == 1:
         return res[0]
 
-    res = re.findall(r'RO_\d*', s)
+    res = re.findall(r'[BFR]{1,2}O_\d*', s)
     if len(res) == 1:
         return res[0]
 
     res = re.findall(r'N\w{32}', s)
-    res = re.findall(r'N\w{32}', s)
     if len(res) == 1:
         return np.NaN
+    '''
 
     return s
 
 rdfFile = './rules/KG_RDF.csv'
-rdfFIle_filtered = './rules/KG_RDF_filter.csv'
+rdfFile_filtered = './rules/KG_RDF_filter.csv'
+prdcFile = './rules/predicates.csv'
 
 RDF = pd.DataFrame()
+RDF_filtered = pd.DataFrame()
 
 if not os.path.exists(rdfFile):
     g = Graph()
-    g.parse('./rules/go-plus.owl')
+    g.parse('./rules/go.rdf')
     
     lst_s = []
     lst_p = []
@@ -49,24 +60,31 @@ if not os.path.exists(rdfFile):
     
     RDF.to_csv(rdfFile, index=False)
 
-else:
-    GO_pattern = r'GO_\d+'
-    RO_pattern = r'RO_\d+'
 
-    RDF = pd.read_csv(rdfFile)
+#GO_pattern = r'GO_\d+'
+GO_pattern = r'[GBFR]{1,2}O_\d+'
 
-    mask = RDF['subject'].str.contains(GO_pattern) | RDF['object'].str.contains(GO_pattern)
-    RDF = RDF[mask]
-    mask = RDF['predicate'].str.contains(RO_pattern)
-    RDF = RDF[mask]
-    
-    RDF.reset_index(drop=True, inplace=True)
-    print(RDF)
-    
-    RDF['subject'] = RDF['subject'].apply(findID)
-    RDF['object'] = RDF['object'].apply(findID)
-    RDF['predicate'] = RDF['predicate'].apply(findID)
-    RDF.dropna(subset=['subject', 'object'], inplace=True)
+RDF = pd.read_csv(rdfFile)
 
-    print(RDF)
-    RDF.to_csv(rdfFIle_filtered, index=False)
+mask = RDF['subject'].str.contains(GO_pattern) |\
+        RDF['object'].str.contains(GO_pattern) | RDF['predicate'].str.contains(GO_pattern)
+RDF = RDF[mask]
+#mask = RDF['predicate'].str.contains(RO_pattern)
+#RDF = RDF[mask]
+
+RDF.reset_index(drop=True, inplace=True)
+print(RDF)
+
+RDF['subject'] = RDF['subject'].apply(findID)
+RDF['object'] = RDF['object'].apply(findID)
+RDF['predicate'] = RDF['predicate'].apply(findID)
+RDF.dropna(subset=['subject', 'object'], inplace=True)
+
+predicates = list(set(RDF['predicate']))
+predicates = pd.Series(predicates)
+predicates.to_csv(prdcFile, index=False)
+
+
+
+print(RDF)
+RDF.to_csv(rdfFile_filtered, index=False)
