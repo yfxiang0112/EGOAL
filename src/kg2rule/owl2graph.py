6ff39c1bff1786d4f99ca238e03c929e454b3b49
221300012-Xiang Yuanfang <yf.xiang@smail.nju.edu.cn>
2024-07-18 17:14:28 +0800

    Jul18 modifying KG2Rule to OOP

--- src/kg2rule/owl2graph.py
+++ src/kg2rule/owl2graph.py
@@ -3,6 +3,49 @@ import pandas as pd
 import os
 import re
 import numpy as np
+from tqdm import tqdm
+
+class KG2Rule():
+
+    def __init__(self, rdf_pth, owl_pth = '', ) -> None:
+        if not os.path.exists(rdf_pth):
+            if owl_pth=='':
+                raise Exception('Need specify path to OWL file when owl is not parsed.')
+
+            self.readOWL(owl_pth, rdf_pth)
+
+        self.rdf = pd.read_csv(rdf_pth)
+
+
+
+    def readOWL(self, owl_pth :str, out_pth :str):
+    # read owl file as rdf graph
+
+        g = Graph()
+        g.parse(owl_pth)
+        
+        # Create Dict of s,p,o
+        df = {'subject':[], 'predicate':[], 'object':[]}
+        for s,p,o in tqdm(g, 'parsing OWL file'):
+            df['subject'].append(s)
+            df['predicate'].append(p)
+            df['object'].append(o)
+        
+        df = pd.DataFrame(df)
+        
+        df.to_csv(out_pth, index=False)
+
+
+
+    def subGraph(self, con_spec_pth :str, exclude_prdc_pth: str, d :int):
+    # Extract Subgraph from parsed RDF (ABL-KG chap 4.3)
+    # exclude unused predicates
+
+        con_spec = list(pd.read_csv(con_spec_pth)['0'])
+        exclude_prdc = list(pd.read_csv(exclude_prdc_pth)['0'])
+
+
+        
 
 def findID(s):
     #TODO: process r'N\w{32}'
@@ -12,8 +55,8 @@ def findID(s):
     idx = 0
     for i in range(len(s)):
         if s[i] == '/':
-            idx = i
-    s = s[idx+1:]
+            idx = i+1
+    s = s[idx:]
     return s
 
 
@@ -33,33 +76,29 @@ def findID(s):
 
     return s
 
-rdfFile = './rules/KG_RDF.csv'
-rdfFile_filtered = './rules/KG_RDF_filter.csv'
-prdcFile = './rules/predicates.csv'
+if __name__ == 'main':
+    rdfFile_filtered = './rules/KG_RDF_filter.csv'
+    prdcFile = './rules/predicates.csv'
+
+    graph = KG2Rule(rdf_pth='./rules/KG_RDF.csv', owl_pth='./rules/go.owl')
+
+'''
+def prdcFilter(s):
+    s = str(s)
+    prdc_to_rm = {'oboInOwl#hasDbXref', 'rdf-schema#label', 'IAO_0000233', 'RO_0002161',\
+                  'oboInOwl#default-namespace', '', '', '',\
+                 }
+
+    if s in prdc_to_rm :
+        return np.NaN
+
+    return s
+
 
 RDF = pd.DataFrame()
 RDF_filtered = pd.DataFrame()
 
 if not os.path.exists(rdfFile):
-    # parse owl file as rdf graph
-    g = Graph()
-    g.parse('./rules/go.owl')
-    
-    lst_s = []
-    lst_p = []
-    lst_o = []
-    
-    print(len(g))
-    for s,p,o in g:
-        lst_s.append(s)
-        lst_p.append(p)
-        lst_o.append(o)
-    
-    RDF['subject'] = lst_s
-    RDF['predicate'] = lst_p
-    RDF['object'] = lst_o
-    
-    RDF.to_csv(rdfFile, index=False)
 
 
 #GO_pattern = r'GO_\d+'
@@ -86,7 +125,6 @@ predicates = list(set(RDF['predicate']))
 predicates = pd.Series(predicates)
 predicates.to_csv(prdcFile, index=False)
 
-
-
 print(RDF)
 RDF.to_csv(rdfFile_filtered, index=False)
+'''
