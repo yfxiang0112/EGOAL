3c1cc802a064f1e850315c61fbe08db740b7298d
221300012-Xiang Yuanfang <yf.xiang@smail.nju.edu.cn>
2024-08-16 22:41:10 +0800

    Aug 16 updated importance & onehot dataset with new concepts

--- scripts/data/update_concept.py
+++ scripts/data/update_concept.py
@@ -2,12 +2,16 @@ import pandas as pd
 
 raw_dataset = pd.read_csv('dataset/raw/dataset.csv')
 
-deg_dataset = pd.read_csv('dataset/deg/dataset.csv')
+#deg_dataset = pd.read_csv('dataset/deg/dataset.csv')
 importance_dataset = pd.read_csv('dataset/importance/processed_dataset_with_importance.csv')
+onehot_dataset = pd.read_csv('dataset/one-hot/one_hot_pro_y_allone.csv')
+onehot_dataset.drop(columns='Unnamed: 0', axis=1, inplace=True)
 
-deg_dataset['CONCEPTS'] = raw_dataset['CONCEPTS']
+#deg_dataset['CONCEPTS'] = raw_dataset['CONCEPTS']
 importance_dataset['CONCEPTS'] = raw_dataset['CONCEPTS']
+onehot_dataset['CONCEPTS'] = raw_dataset['CONCEPTS']
 
 
-deg_dataset.to_csv('dataset/deg/dataset.csv', index=False)
+#deg_dataset.to_csv('dataset/deg/dataset.csv', index=False)
 importance_dataset.to_csv('dataset/importance/processed_dataset_with_importance.csv', index=False)
+onehot_dataset.to_csv('dataset/one-hot/one_hot_pro_y_allone.csv', index=False)
