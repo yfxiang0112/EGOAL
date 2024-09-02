45fd7a1a8ee047d794ec8d033d906b80964ac78b
221300012-Xiang Yuanfang <yf.xiang@smail.nju.edu.cn>
2024-09-02 20:26:55 +0800

    Sep 2 predict added command line args

--- src/predict.py
+++ src/predict.py
@@ -1,3 +1,4 @@
+import sys, getopt
 import pandas as pd
 import joblib
 import glob
@@ -10,78 +11,102 @@ from sklearn.metrics import roc_curve, auc
 from concept_extract.term_embd import graph_parse
 from concept_extract.txt2con import EmbeddingConverter
 
-in_pth = 'predict/input.txt'
-out_dir = 'predict/results'
-n_inputs = 0
 
-##################################################
+def main(argv):
 
-''' initialize text embedding converter '''
-owl_pth = 'rules/go.owl'
-embd = EmbeddingConverter(owl_pth, use_vpn=True)
+    ''' parse in & out arg '''
 
-''' initialize concept lists '''
-TOP_CNT = 10
-concepts = []
-#con_descps = [[] for _ in range(TOP_CNT)]
+    in_pth = 'predict/input.txt'
+    out_dir = 'predict/results'
+    n_inputs = 0
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","odir="])
+    except getopt.GetoptError:
+        print ('test.py -i <input_dir> -o <output_dir>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('test.py -i <input_file> -o <output_dir>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            in_pth = arg
+        elif opt in ("-o", "--odir"):
+            out_dir = arg
 
-''' read descriptions from input file '''
-descriptions = []
-with open(in_pth, 'r') as f:
-    descriptions = f.readlines()
-    n_inputs = len(descriptions)
-
-''' iterate instances '''
-for d in tqdm(descriptions, 'processing input instance'):
-
-        ''' compute similarity matrix & most similar concept list '''
-        sim = embd.similar_matrix(d)
-        sorted_sim = embd.max_sim(TOP_CNT)
-
-        concepts.append(list(sorted_sim))
-
-
-''' load gene - product mapping '''
-gene_mapping = {}
-with open('predict/gene_mapping.txt', 'r') as f:
-    gene_mapping = eval(f.readline())
-
-
-
-''' load pretrained model for each single gene '''
-models = []
-name_list = []
-model_files = glob.glob('models/SO_*_model.joblib')
-for gene_id in tqdm(range(1, 4759)):
-    model_file = f'models/SO_{gene_id:04d}_model.joblib'
-    name_list.append(f'SO_{gene_id:04d}')
-    if model_file in model_files:
-        model = joblib.load(model_file)
-        models.append(model)
-    else:
-        models.append(None)  
-
-''' predict for each input with pretrained model '''
-for i in tqdm(range(n_inputs), 'processing input'):
-    concept_ids = [int(re.sub(r'GO_0*', '', c)) for c in concepts[i]]
     
-    predictions = []
-    for model in models:
-        if model is not None:
-            prediction = model.predict_proba([concept_ids])[0]
-            predictions.append(prediction)
+    ##################################################
+    
+    ''' initialize text embedding converter '''
+    owl_pth = 'rules/go.owl'
+    embd = EmbeddingConverter(owl_pth, use_vpn=True)
+    
+    ''' initialize concept lists '''
+    TOP_CNT = 10
+    concepts = []
+    #con_descps = [[] for _ in range(TOP_CNT)]
+    
+    ''' read descriptions from input file '''
+    descriptions = []
+    with open(in_pth, 'r') as f:
+        descriptions = f.readlines()
+        n_inputs = len(descriptions)
+    
+    ''' iterate instances '''
+    for d in tqdm(descriptions, 'processing input instance'):
+    
+            ''' compute similarity matrix & most similar concept list '''
+            sim = embd.similar_matrix(d)
+            sorted_sim = embd.max_sim(TOP_CNT)
+    
+            concepts.append(list(sorted_sim))
+    
+    
+    ''' load gene - product mapping '''
+    gene_mapping = {}
+    with open('predict/gene_mapping.txt', 'r') as f:
+        gene_mapping = eval(f.readline())
+    
+   ################################################## 
+    
+    ''' load pretrained model for each single gene '''
+    models = []
+    name_list = []
+    model_files = glob.glob('models/SO_*_model.joblib')
+    for gene_id in tqdm(range(1, 4759)):
+        model_file = f'models/SO_{gene_id:04d}_model.joblib'
+        name_list.append(f'SO_{gene_id:04d}')
+        if model_file in model_files:
+            model = joblib.load(model_file)
+            models.append(model)
         else:
-            predictions.append([0,0])  
+            models.append(None)  
     
-    ''' zip name & pred prob as lst '''
-    result = [(name, pred[1]) for name, pred in zip(name_list, predictions) if pred[1] > .5]
-    result.sort(key= lambda x: x[1], reverse=True)
+    ''' predict for each input with pretrained model '''
+    for i in tqdm(range(n_inputs), 'processing input'):
+        concept_ids = [int(re.sub(r'GO_0*', '', c)) for c in concepts[i]]
+        
+        predictions = []
+        for model in models:
+            if model is not None:
+                prediction = model.predict_proba([concept_ids])[0]
+                predictions.append(prediction)
+            else:
+                predictions.append([0,0])  
+        
+        ''' zip name & pred prob as lst '''
+        result = [(name, pred[1]) for name, pred in zip(name_list, predictions) if pred[1] > .5]
+        result.sort(key= lambda x: x[1], reverse=True)
+        
+        with open(f'{out_dir}/res_{i}.txt', 'w') as f:
+            f.write('gene id\tconfidence\tproduct\n')
+            for g in result:
+                prod = gene_mapping[g[0]]
+                if 'unknown' in prod or 'uncharacterized' in prod:
+                    continue
     
-    with open(f'{out_dir}/res_{i}.txt', 'w') as f:
-        f.write('gene id\tconfidence\tproduct\n')
-        for g in result:
-            prod = gene_mapping[g[0]]
-            if 'unknown' in prod or 'uncharacterized' in prod:
-                continue
+                f.write(f'{g[0]}\t{g[1]:.2f}\t{gene_mapping[g[0]]}\n')
 
-            f.write(f'{g[0]}\t{g[1]:.2f}\t{gene_mapping[g[0]]}\n')
+    ##################################################
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
