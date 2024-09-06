320e0766e0115d9112c7e11969f87dce60f5a0ac
221300012-Xiang Yuanfang <yf.xiang@smail.nju.edu.cn>
2024-09-06 10:15:03 +0800

    Sep 6 updated rule with unlabel data

--- scripts/concepts/get_domain.py
+++ scripts/concepts/get_domain.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-con_df = pd.read_csv('dataset/concepts/GSE_concepts.csv')
+label_df = pd.read_csv('dataset/importance/dataset_onehot.csv')
+unlabel_df = pd.read_csv('dataset/unlabel/dataset_unlabel.csv')
+
 goa_df = pd.read_csv('rules/goa_mapping.csv', header=None)
 all_con = pd.read_csv('dataset/concepts/all_concept_name.txt', header=None)
 all_con = list(all_con[0])
@@ -10,13 +12,17 @@ all_con = list(all_con[0])
 con_dom = set()
 con_dom_data = set()
 
-for con_lst in con_df['CONCEPTS']:
+for con_lst in label_df['CONCEPTS']:
     for s in eval(con_lst):
-        #con_dom.add(s)
-        #NOTE: tmp operation, transfer GO:xxxx to GO_xxxx
         if s in all_con:
-            con_dom.add('GO_'+s[3:])
-            con_dom_data.add('GO_'+s[3:])
+            con_dom.add(s)
+            con_dom_data.add(s)
+
+for con_lst in unlabel_df['CONCEPTS']:
+    for s in eval(con_lst):
+        if s in all_con:
+            con_dom.add(s)
+            con_dom_data.add(s)
 
 for con in goa_df[0]:
     con_dom.add(con)
@@ -26,6 +32,8 @@ with open('dataset/concepts/concept_dom.txt', 'w') as f:
 
 with open('dataset/concepts/data_con_dom.txt', 'w') as f:
     f.write(str(con_dom_data))
+
+print(f'len of concept domain:{len(con_dom)}, len of concept subdomain from dataset: {len(con_dom_data)}')
 #con_dom = list(con_dom)
 #con_dom.sort()
 #
