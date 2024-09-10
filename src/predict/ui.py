1ca7d448fca4166104b3c67ff962492ae1f34622
RAINYWJT <798250397@qq.com>
2024-09-10 17:05:14 +0800

    ui

--- src/predict/ui.py
+++ src/predict/ui.py
@@ -58,29 +58,37 @@ def main():
     out_dir_qut = f"'{out_dir}'"
     # print(in_pth_qut, out_dir_qut)
     put_text("正在预测中，请稍后")
-    # predict(in_pth_qut, out_dir_qut)
+    #predict(in_pth_qut, out_dir_qut)
 
     clear()
     put_markdown(r""" # <center> <font face="楷体"> 基于反绎学习和基因知识库的基因表达预测 </font> </center>
     """)
     path = out_dir
     put_text("基因表达结果预测如下:")
-    for filename in os.listdir(out_dir):
+    
+    txt_files = [f for f in os.listdir(out_dir) if f.endswith(".txt")]
+    txt_files.sort(key=lambda x: int(x.split('_')[-1].split('.')[0]))
+    
+    for filename in txt_files:
         if filename.endswith(".txt"):
             file_path = os.path.join(out_dir, filename)
             with open(file_path, 'r', encoding='utf-8') as file:
                 content = file.read()
                 put_text(f"文件名: {filename}")
                 put_text(content)
-    
+                
+                # 调用 plt 函数进行画图
+                plot(out_dir)
+                image_filename = f"{filename.split('.')[0]}_plot.png"
+                image_path = os.path.join(out_dir, image_filename)
+                put_text(f"图表: {image_filename}")
+                put_image(open(image_path, 'rb').read())
 
-    # 调用 plt 函数进行画图
-    plot(out_dir)
-    for filename in os.listdir(out_dir):
-        if filename.endswith("_plot.png"):
-            image_path = os.path.join(out_dir, filename)
-            put_text(f"图表: {filename}")
-            put_image(open(image_path, 'rb').read())
+                graph_filename = f"res_{filename.split('.')[0].split('_')[-1]}_graph.png"
+                image_path = os.path.join(out_dir, graph_filename)
+                put_text(f"关系图表: {graph_filename}")
+                put_image(open(image_path, 'rb').read())
+                put_text('------------------------------------------------------------------------------------------------------------------')
 
 if __name__ == '__main__':
     main()
